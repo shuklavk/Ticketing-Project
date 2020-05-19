9bf7a4debeab36926e30e57a2b049c6a2a333d4b
Vikas Shukla <shuk.vk@gmail.com>
2020-05-19 15:05:34 -0700

    adds routes for auth service

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,14 +1,19 @@
 import express from 'express';
 import { json } from 'body-parser';
+import { currentUserRouter } from './routes/current-user';
+import { signInRouter } from './routes/signin';
+import { signOutRouter } from './routes/signout';
+import { signUpRouter } from './routes/signup';
 
 const app = express();
 const port = 3000;
 
 app.use(json());
 
-app.get('/api/users/currentUser', (req, res) => {
-  res.send('Hi there!');
-});
+app.use(currentUserRouter);
+app.use(signInRouter);
+app.use(signOutRouter);
+app.use(signUpRouter);
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
