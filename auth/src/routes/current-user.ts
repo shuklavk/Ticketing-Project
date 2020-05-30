06a439c1744cdaea402a897d9fdf6aa76e4ae1e8
Vikas Shukla <shuk.vk@gmail.com>
2020-05-30 10:12:39 -0700

    implments /currentuser route

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,22 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
+// This route is a check to see if user is logged in
+// Returns {currentUser: null} if user is not logged in
+// else it returns {currentUser: payload} and payload has the email/id/iat
 router.get('/api/users/currentUser', (req, res) => {
-  res.send('hi there from current user');
+  if (!req.session || !req.session.jwt) {
+    return res.send({ currentUser: null });
+  }
+
+  try {
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+    res.send({ currentUser: payload });
+  } catch {
+    res.send({ currentUser: null });
+  }
 });
 
 export { router as currentUserRouter };
