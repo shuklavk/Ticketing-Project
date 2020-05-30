1620dfe7f8c1f72e68746402e4922bfd8f09bb4f
Vikas Shukla <shuk.vk@gmail.com>
2020-05-30 16:20:05 -0700

    adds helper function for signing up for tests

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -3,7 +3,15 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
 import { app } from '../app';
+import request from 'supertest';
 
+declare global {
+  namespace NodeJS {
+    interface Global {
+      signup(): Promise<string[]>;
+    }
+  }
+}
 let mongo: any;
 
 // before all tests
@@ -33,3 +41,19 @@ afterAll(async () => {
   await mongo.stop();
   await mongoose.connection.close();
 });
+
+global.signup = async () => {
+  const email = 'test@test.com';
+  const password = 'password';
+
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({
+      email,
+      password
+    })
+    .expect(201);
+
+  const cookie = response.get('Set-Cookie');
+  return cookie;
+};
